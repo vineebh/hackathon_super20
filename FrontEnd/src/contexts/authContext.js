e850860aff5952a96919453f52d928ddec2d2ef6
tryHacksuper20 <shubhamyadav442003@gmail.com>
2024-09-13 19:11:57 +0530

    Sachin (#44)
    Login bug solved

--- FrontEnd/src/contexts/authContext.js
+++ FrontEnd/src/contexts/authContext.js
@@ -1,38 +1,41 @@
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useContext, useEffect, useState } from "react";
-import { auth } from "../../firebase/firebase";
+import { auth } from "../firebase/firebase";
+
 
 const AuthContext = React.createContext();
-
 export function useAuth() {
   return useContext(AuthContext);
 }
 
+
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null); 
+  const [userLoggedIn, setUserLoggedIn] = useState(false); 
   const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setCurrentUser(user);
-        setUserLoggedIn(true);
+        setUserLoggedIn(true); 
       } else {
-        setCurrentUser(null);
+        setCurrentUser(null); 
         setUserLoggedIn(false);
       }
       setLoading(false);
     });
 
-    return unsubscribe;
+    return unsubscribe; 
   }, []);
 
   const value = {
-    currentUser,
-    userLoggedIn,
+    currentUser,   
+    userLoggedIn, 
     loading,
   };
+
 
   return (
     <AuthContext.Provider value={value}>
@@ -40,3 +43,5 @@ export function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
+
+export default AuthProvider;
