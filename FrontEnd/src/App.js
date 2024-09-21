5b1c32d258fb469361af0a8a40253a8378f8b24c
tryHacksuper20 <shubhamyadav442003@gmail.com>
2024-09-21 23:12:17 +0530

    speed (#65) (#66)
    Co-authored-by: Sachin Yadav <96512148+101-sachin@users.noreply.github.com>

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Header from "./components/Header";
+import { AuthProvider } from "./contexts/authContext";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
-import Header from "./components/Header";
-import Home from "./Pages/Home";
-import Auth from "./components/Auth/Auth";
-import { AuthProvider } from "./contexts/authContext"; 
-import Test from "./Pages/Test";
 import Courses from "./Pages/Courses";
+import Home from "./Pages/Home";
+import Test from "./Pages/Test";
+import Exam from "./Pages/MCQ";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
             <Route path="/auth" element={<Auth />} />
             <Route path="/test" element={<Test/>}/>
             <Route path="/courses" element={<Courses/>}/>
+            <Route path="/mcq" element={<Exam/>}/>
           </Routes>
         </main>
       </div>
